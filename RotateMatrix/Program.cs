0e93ce7dce3a692bd3dedd1c00138066ceecc0d5
sandman633 <69395587+sandman633@users.noreply.github.com>
2022-02-09 23:56:44 +0300

    Добавьте файлы проекта.

---
+++ RotateMatrix/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Matrix = RotateMatrix.RotateMatrix;
+Console.WriteLine("Hello, World!");
+
+
+Matrix RotateMatrix = new Matrix();
+RotateMatrix.printMessage += PrintMessage;
+
+Console.WriteLine(RotateMatrix.LogMatrix(RotateMatrix.matrix));
+
+Console.WriteLine("Start Rotating --------------------------");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+RotateMatrix.Rotate(RotateMatrix.matrix);
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("End Rotating --------------------------");
+
+
+Console.ReadKey();
+
+void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
---
+++ RotateMatrix/RotateMatrix.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateMatrix
+{
+    public class RotateMatrix
+    {
+        public readonly int[,] matrix = new int[,]
+        {
+            { 1, 4, 0, 5, 9 },
+            { 2, 7, 3, 4, 1 },
+            { 0, 3, 8, 1, 0 },
+            { 3, 2, 9, 5, 3 },
+            { 6, 4, 0, 1, 7 },
+        };
+        public event Action<string> printMessage;
+
+
+        public void Rotate(int[,] matrix)
+        {
+            var n = matrix.GetLength(0);
+            for (int layer = 0; layer < n / 2; layer++)
+            {
+                var first = layer;
+                var last = n - 1 - layer;
+                for (int i = first; i < last; i++)
+                {
+                    var offset = i - first;
+                    var upLeft = matrix[first, i];
+                    var downLeft = matrix[last - offset, first];
+                    var downRight = matrix[last, last - offset];
+                    var upRight = matrix[i, last];
+
+
+                    matrix[first, i] = upRight;
+                    matrix[last - offset, first] = upLeft;
+                    matrix[last, last - offset] = downLeft;
+                    matrix[i, last] = downRight;
+
+
+                    printMessage?.Invoke(LogMatrix(matrix)); 
+                    
+
+
+                }
+
+            }
+        }
+
+        public string LogMatrix(int[,] matrix)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sb.Append("[ ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sb.Append(" ");
+                    sb.Append(matrix[i, j]);
+                    sb.Append(" ");
+                }
+                sb.Append(" ]");
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
